d527ce865ce419996b6f47c284fc9735bc94f558
Kabelo Martin Mayisa <kabelomayisa@outlook.com>
2019-05-06 16:46:07 +0200

    .

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //selecting and getting hold of the right elements
 const button = document.querySelector('.hiddingButton');
-const list  = document.querySelector('.list');
+const list = document.querySelector('.list');
 
 //adding an event listener and making changes to button triggers
-button.addEventListener('click',() => {
+button.addEventListener('click', () => {
 
-    if(list.style.display == 'none'){
+    if (list.style.display == 'none') {
         button.textContent = 'Hide List';//change the button's text
         list.style.display = 'block';//displaying the list
     }
-    else{
+    else {
         button.textContent = 'Show List';//changing the button's text
         list.style.display = 'none';//hidding the list
     }
